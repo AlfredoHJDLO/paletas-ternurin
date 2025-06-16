933634559539b86faadd0bcdb71f8325561405e2
saulpena636 <suoerpenniya@gmail.com>
2025-06-16 07:20:25 -0600

    fix: responsivo1

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import shopping from '../assets/shopping.png';
 import "./header.css"
 
 function Header() {
   const navigate = useNavigate();
+  const [menuAbierto, setMenuAbierto] = useState(false);
 
   const logout = () => {
     localStorage.removeItem('token');
@@ -15,24 +16,31 @@ function Header() {
   };
 
   return (
-    <header>
-      <div className='title'>
+  <header>
+    <div className='title'>
+      <Link to="/" style={{ textDecoration: 'none', color: 'inherit' }}>
         Ternurin
-      </div>
-      <nav>
-        <ul className='ul1' style={{
-          listStyle: 'none',
-          margin: 0,
-          padding: 0,
-          display: 'flex',
-          gap: '30px'
-        }}>
-          <li className='li1'><Link to="/" className="ah">Inicio</Link></li>
-          <li className='li1'><a className="ah" href="#productos" >Productos</a></li>
-          <li className='li1'><Link to="/nosotros" className="ah">Sobre nosotros</Link></li>
-          <li className='li1'><Link to="/contacto" className="ah">Contacto</Link></li>
-          {localStorage.getItem('Id') !== null ? (
-            <li className="dropdown li1">
+      </Link>
+    </div>
+
+    {/* Botón de hamburguesa para móviles */}
+    <button 
+      className="menu-hamburguesa" 
+      onClick={() => setMenuAbierto(!menuAbierto)}
+    >
+      ☰
+    </button>
+
+    {/* Añadimos la clase 'nav-abierto' cuando el estado es true */}
+    <nav className={menuAbierto ? 'nav-abierto' : ''}>
+      <ul className='ul1'>
+        <li className='li1'><Link to="/" className="ah">Inicio</Link></li>
+        <li className='li1'><a className="ah" href="#productos">Productos</a></li>
+        <li className='li1'><Link to="/nosotros" className="ah">Sobre nosotros</Link></li>
+        <li className='li1'><Link to="/contacto" className="ah">Contacto</Link></li>
+        
+        {localStorage.getItem('Id') !== null ? (
+          <li className="dropdown li1">
             <span>{localStorage.getItem('user')} ▾</span>
             <ul className="dropdown-menu ul1">
               <li className='li1'>Mi cuenta</li>
@@ -40,18 +48,19 @@ function Header() {
               <li className='li1' onClick={logout}>Cerrar sesión</li>
             </ul>
           </li>
-          ):
-          (<li className='li1'><Link to='/login'>Iniciar sesion</Link></li>)}
-          
-        </ul>
-      </nav>
-      <div style={{ height: '64px', color: '#9E6D4E' }}>
-        <Link to="/carrito" style={{ textDecoration: 'none', color: 'inherit' }}>
-          <img src={shopping} alt="Carrito" /> {/* Icono de carrito */}
-        </Link>
-      </div>
-    </header>
-  );
+        ) : (
+          <li className='li1'><Link to='/login' className="ah">Iniciar sesión</Link></li>
+        )}
+      </ul>
+    </nav>
+
+    <div className="carrito-container">
+      <Link to="/carrito" style={{ textDecoration: 'none', color: 'inherit' }}>
+        <img src={shopping} alt="Carrito" />
+      </Link>
+    </div>
+  </header>
+);
 }
 
 export default Header;
